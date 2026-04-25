9f62dd05085c9a3927c78025527b7ab8a638c05f
Jose Bocanegra <josebv107@gmail.com>
2026-04-25 18:09:32 -0500

    Creé los hijos de pato y las uní mediante herencia

---
+++ src/simupatolab2/PatoSenuelo.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package simupatolab2;
+
+/**
+ *
+ * @author sistemas
+ */
+public class PatoSenuelo extends Pato{
+    
+}
