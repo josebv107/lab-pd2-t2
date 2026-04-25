e9129d149b7cc025ae8f61e44bcee4be3d455dc5
Jose Bocanegra <josebv107@gmail.com>
2026-04-25 18:00:58 -0500

    Cree las clases pato, volable y cuaqueable y las uní

---
+++ src/simupatolab2/Pato.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package simupatolab2;
+
+/**
+ *
+ * @author sistemas
+ */
+public abstract class Pato {
+    private Volable volable;
+    private Cuaqueable cuaqueable;
+    
+    public Pato() {
+        
+    }
+    
+    public void setVolable() {
+        
+    }
+    
+    
+}
---
+++ src/simupatolab2/Volable.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package simupatolab2;
+
+/**
+ *
+ * @author sistemas
+ */
+public interface Volable {
+    public void cuaquear();
+}
